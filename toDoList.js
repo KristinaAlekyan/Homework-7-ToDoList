16443e50ff5636f48ffda24e80030132b4c5fa17
KristinaAlekyan <53223679+KristinaAlekyan@users.noreply.github.com>
2019-11-07 01:04:58 +0400

    Update toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -1,7 +1,28 @@
-
+let idLi;
+let idDeletBut;
 function addtask(){
     let nextTask = document.getElementById('textbox').value;
     let box = document.createElement("li");
-    box.innerHTML = nextTask;
-    document.getElementById("list").appendChild(box);
+        box.innerHTML = nextTask;
+        idLi = ""+nextTask;
+        box.className="box";
+        box.id=idLi;
+        document.getElementById("list").append(box);
+    
+    
+    let delButton=document.createElement("button");
+        delButton.className = "delete";
+        idDeletBut=idLi+1;
+        delButton.innerHTML = "delete";
+        document.getElementById(idLi).append(delButton);
+        delButton.addEventListener('click',function(){
+        deletTask(this);
+  })
+    
+}
+
+function deletTask(el){
+    let removEl=el.parentNode;
+    removEl.remove();
+    
 }
